1261db80acda7e37b07c7f639dc1a3c8d9c164e9
Archit Pandit <architpandit90@gmail.com>
2022-09-30 17:34:53 -0400

    Fixed spell button image = null bug
    Fixed the annoying bug where the image component on the spell button game object would always null even if I assigned it in code and saw it in the inspector. Eventually had to serialize the variable and attach it in the inspector.

--- Magic Survival-esque/Assets/Scripts/Player/Spells/SpellButton.cs
+++ Magic Survival-esque/Assets/Scripts/Player/Spells/SpellButton.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 public class SpellButton : MonoBehaviour
 {
     private ItemObj spell;
-    private Image image;
+    [SerializeField] Image image;
 
     // Start is called before the first frame update
     void Start()
     {
-        image = GetComponent<Image>();
+        //image = GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -23,13 +23,12 @@ public class SpellButton : MonoBehaviour
 
     public void OnInstantiate(ItemObj newSpell)
     {
-        Debug.Log(newSpell);
-
         spell = newSpell;
 
         if(image != null)
         {
             image.sprite = spell.icon;
+            image.enabled = true;
         }
     }
 }
